2ac6129fb70dfa39fad0e412b311e16a47260a24
Darickson18 <dleiva@correo.unimet.edu.ve>
2025-03-24 08:09:15 -0400

    Update Pregunta 3.py

---
+++ Pregunta 3.py
@@ -0,0 +1,17 @@
+class persona:
+    def __init__( nombre, edad):
+        nombre = nombre
+        edad = edad
+
+def saludar (self):
+    return f"Hola, soy {self.nombre}, y tengo {self. edad} anos "
+
+p = Personal("carlos", 25)
+print(p.saludar())
+
+
+¿Cuál es el bug en este código?
+1. Falta definir la clase hija que hereda de Persona.
+2. El método __init__ está utilizando parámetros incorrectos. Respuesta correcta
+3. No se ha definido correctamente el método descripcion().
+4. La instancia mi_persona está mal nombrada en la creación del objeto.
